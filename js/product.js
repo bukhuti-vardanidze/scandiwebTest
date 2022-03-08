8f70ce3cc7018dbdf077274ac2c1ae0f27edac4a
bukhuti-vardanidze <vardanidzebuta6@gmail.com>
2022-03-08 16:42:08 +0400

    page-2

---
+++ js/product.js
@@ -0,0 +1,33 @@
+
+/*
+
+    let check = document.getElementById('check')
+    let btn = document.getElementById('btn')
+   btn.addEventListener('click', function (){
+    if(check.checked){
+        btn.removeAttribute('disable')
+
+    } else {
+        btn.disabled='true'
+    }
+   })
+ 
+ 
+ 
+  function enable(){
+    let check = document.getElementById('check')
+    let btn = document.getElementById('btn')
+    if(check.checked){
+        btn.removeAttribute('disable')
+
+    } else {
+        btn.disabled='true'
+    }
+}
+
+let navbarLinks = document.getElementById('navBarlinks');
+let Burger = document.getElementById('burgerbar');
+
+Burger.addEventListener('click', function()  {
+      navbarLinks.classList.toggle('active');
+    })   */
