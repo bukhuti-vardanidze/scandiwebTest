64f7ddad52747b1ea83add3f9d37772d4339ec03
bukhuti-vardanidze <vardanidzebuta6@gmail.com>
2022-03-13 20:34:16 +0400

    js-functions

--- js/product.js
+++ js/product.js
@@ -1,33 +1,181 @@
-
-/*
-
-    let check = document.getElementById('check')
-    let btn = document.getElementById('btn')
-   btn.addEventListener('click', function (){
-    if(check.checked){
-        btn.removeAttribute('disable')
-
+                /* dvd-check */
+function enable(){
+ var  productCheck = document.getElementById('checkA');
+ var  addButton = document.getElementById('addDel')
+    if(productCheck.checked == true){
+        addButton.style.display = 'block'
     } else {
-        btn.disabled='true'
-    }
-   })
- 
- 
- 
-  function enable(){
-    let check = document.getElementById('check')
-    let btn = document.getElementById('btn')
-    if(check.checked){
-        btn.removeAttribute('disable')
+        addButton.style.display = 'none'
 
-    } else {
-        btn.disabled='true'
     }
 }
 
-let navbarLinks = document.getElementById('navBarlinks');
-let Burger = document.getElementById('burgerbar');
 
-Burger.addEventListener('click', function()  {
-      navbarLinks.classList.toggle('active');
-    })   */
+function enableB(){
+    var  productCheck = document.getElementById('checkB');
+    var  addButton = document.getElementById('addDel')
+       if(productCheck.checked == true){
+           addButton.style.display = 'block'
+       } else {
+           addButton.style.display = 'none'
+   
+       }
+   }
+
+   function enableC(){
+    var  productCheck = document.getElementById('checkC');
+    var  addButton = document.getElementById('addDel')
+       if(productCheck.checked == true){
+           addButton.style.display = 'block'
+       } else {
+           addButton.style.display = 'none'
+   
+       }
+   }
+
+
+   function enableD(){
+    var  productCheck = document.getElementById('checkD');
+    var  addButton = document.getElementById('addDel')
+       if(productCheck.checked == true){
+           addButton.style.display = 'block'
+       } else {
+           addButton.style.display = 'none'
+   
+       }
+   }
+
+
+                          /*book-ck */
+
+    function enableSecA(){
+    var  productCheck = document.getElementById('checkSec1');
+    var  addButton = document.getElementById('addDel')
+    if(productCheck.checked == true){
+        addButton.style.display = 'block'
+    } else {
+        addButton.style.display = 'none'
+
+    }
+}
+
+function enableSecB(){
+    var  productCheck = document.getElementById('checkSec2');
+    var  addButton = document.getElementById('addDel')
+    if(productCheck.checked == true){
+        addButton.style.display = 'block'
+    } else {
+        addButton.style.display = 'none'
+
+    }
+}
+
+
+function enableSecC(){
+    var  productCheck = document.getElementById('checkSec3');
+    var  addButton = document.getElementById('addDel')
+    if(productCheck.checked == true){
+        addButton.style.display = 'block'
+    } else {
+        addButton.style.display = 'none'
+
+    }
+}
+
+function enableSecD(){
+    var  productCheck = document.getElementById('checkSec4');
+    var  addButton = document.getElementById('addDel')
+    if(productCheck.checked == true){
+        addButton.style.display = 'block'
+    } else {
+        addButton.style.display = 'none'
+
+    }
+}
+
+                           /* furniture */
+
+
+function enablethrA(){
+    var  productCheck = document.getElementById('checkthr1');
+    var  addButton = document.getElementById('addDel')
+    if(productCheck.checked == true){
+        addButton.style.display = 'block'
+    } else {
+        addButton.style.display = 'none'
+
+    }
+}   
+
+
+function enablethrB(){
+    var  productCheck = document.getElementById('checkthr2');
+    var  addButton = document.getElementById('addDel')
+    if(productCheck.checked == true){
+        addButton.style.display = 'block'
+    } else {
+        addButton.style.display = 'none'
+
+    }
+}   
+
+
+
+function enablethrC(){
+    var  productCheck = document.getElementById('checkthr3');
+    var  addButton = document.getElementById('addDel')
+    if(productCheck.checked == true){
+        addButton.style.display = 'block'
+    } else {
+        addButton.style.display = 'none'
+
+    }
+}   
+
+
+
+function enablethrD(){
+    var  productCheck = document.getElementById('checkthr4');
+    var  addButton = document.getElementById('addDel')
+    if(productCheck.checked == true){
+        addButton.style.display = 'block'
+    } else {
+        addButton.style.display = 'none'
+
+    }
+}   
+
+
+
+   /*prduct list */
+
+   /*list */
+
+  function getDVDList(){
+   var x = document.getElementById('list').value;
+   if(x == "DVD"){
+      document.getElementById('dvdFun').style.display = 'block'
+      document.getElementById('bookFun').style.display = 'none'
+      document.getElementById('furnitureFun').style.display = 'none'
+    } else 
+        if(x == "Book"){
+           document.getElementById('dvdFun').style.display = 'none'
+           document.getElementById('bookFun').style.display = 'block'
+            document.getElementById('furnitureFun').style.display = 'none'
+    } else 
+    if(x == "Furniture"){
+        document.getElementById('dvdFun').style.display = 'none'
+        document.getElementById('bookFun').style.display = 'none'
+         document.getElementById('furnitureFun').style.display = 'block'
+       }
+    else
+    if(x == "select"){
+        document.getElementById('dvdFun').style.display = 'none'
+        document.getElementById('bookFun').style.display = 'none'
+         document.getElementById('furnitureFun').style.display = 'none'
+       }
+
+  
+  }
+ 
+ 
